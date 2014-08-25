b5c2e458c300360b04018d6baeb02cdc0f1e4681
hbar <haroldbarnard@gmail.com>
2014-08-25 14:07:49 -0400

    completed bethe bloch function for ion stopping, sitll requires testing

--- lib/ChargedParticleTools/BetheBloch.py
+++ lib/ChargedParticleTools/BetheBloch.py
@@ -1,17 +1,22 @@
+## Implementation of the bethe bloch equation to calculate stopping power for ions
+
 from numpy import *
+from constants import *
 
-q0 = 1.60217657e-19
-ep0 = 8.85418782e-12
-c0 = 299792458
-Me = 0.510998910e6
-A0 = 6.0221413e+23
 
-def BetheBloch(Beta,zi=1, Z=26, A=56, 
+def IonStopping(beta,element,density,zIon=1) 
 
-	C1 = ((q0**2)/(4.0*pi*ep0))**2
+	C1=[]; C2=[]; C3=[]
 
-	C2 = (4.0 * pi * zi**2 * N0 * Zt * rho)/(Me * A)
+	for i in range(len(element)):
+		Zt = element[i].z
 
-	C3 = 2.0 * 
+		C1.append( ((q0**2)/(4.0*pi*ep0))**2 )
 
-	dEdX = C1 * C2 * Beta**-2 * ( log(C3 * Beta**2) 
+		C2.apped( (4.0 * pi * zIon**2 * density[i] * Zt * rho)/(Me * A) )
+
+		C3.append( 2.0 * me * beta**2 / (element[i].ionization * (1.0-beta) ) )
+	
+		dEdX = dEdZ + C1[i] * C2[i] * beta**-2 * ( log(C3[i] * beta**2) - beta**2 )
+
+	return dEdX
--- lib/ChargedParticleTools/Constants.py
+++ lib/ChargedParticleTools/Constants.py
@@ -6,6 +6,9 @@ me = 0.51099891013e6 # electron rest mass [eV/c^2]
 Me = 9.1093821545e-31  # electron rest mass [kg]
 mp = 938.27204621e6 # proton rest mass [eV/c^2]
 Mp = 1.67262177774e-27 # proton rest mass [kg]
+ep0 = 8.85418782e-12 # vacuum permitivity [F/m]
+mu0 = pi * 4e-7 # vacuum permiability [H/m] or [N/A^2]
+A0 = 6.0221413e+23 # Avogradro's number
 
 
 
